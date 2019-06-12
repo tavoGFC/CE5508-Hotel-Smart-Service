0f26d284b532f4c9a02ed59258976b5a98d79d3d
tavoGFC <gusfcarrera@hotmail.es>
2019-06-12 05:35:06 -0600

    delete zip

--- code_api_db/index.js
+++ code_api_db/index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 443;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
