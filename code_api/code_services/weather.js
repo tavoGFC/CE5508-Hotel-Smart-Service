0881dcb2464eef9a97ea265e611565c8dfc509d5
tavoGFC <gusfcarrera@hotmail.es>
2019-06-14 11:39:41 -0600

    Update weather.js

--- code_api/code_services/weather.js
+++ code_api/code_services/weather.js
@@ -2,17 +2,23 @@ const fetch = require('node-fetch');
 
 async function getWeather(lang) {
 
-  return fetch(`"http://api.apixu.com/v1/forecast.json?key=64700405af534cbe914184115192605&q=9.123185,-83.6931613&days=7&lang=${lang}`)
+  return fetch(`"http://api.apixu.com/v1/forecast.json?key=64700405af534cbe914184115192605&q=9.123185,-83.6931613&days=7&lang=${lang}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
     .then(response => response.json())
     .then(responseJson => {
       if (responseJson != '') {
-        var weather = [];
-        setTimeout(() => {
-          weather = responseJson.forecast.forecastday.map(function (item, index) {
-            return { key: index, date: item.date, temp: item.day.avgtemp_c, condition: item.day.condition.text, icon: 'http://' + item.day.condition.icon };
-          });
-        }, 2000);
-        return { "test": "weather" };
+        const stringJSON = JSON.stringify(responseJson).slice(1, -1);
+        const parseResponse = JSON.parse(stringJSON);
+        const weather = parseResponse.forecast.forecastday.map(function (item, index) {
+          return { key: index, date: item.date, temp: item.day.avgtemp_c, condition: item.day.condition.text, icon: 'http://' + item.day.condition.icon };
+        });
+        console.log(parseResponse);
+        console.log(weather)
+        return weather;
       }
     })
     .catch(error => {
